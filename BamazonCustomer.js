3e96c2431a36833bc4be8e9375766d8ac39335aa
dsuare1 <suarez.derrick@gmail.com>
2016-07-11 17:06:21 -0500

    initial check against user desired quantity vs in-stock quantity working

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,23 +1,66 @@
-var inquirer = require("inquirer");
+var prompt = require("prompt");
 var mysql = require("mysql");
 
+var schema = {
+    properties: {
+        productID: {
+            description: "Please enter the ID of the product you'd like to purchase",
+            pattern: /[1-9|10]/,
+            message: "ProductID must be only numbers 1 - 10",
+            required: true
+        },
+        Quantity: {
+            description: "Ok, got it!  Please enter the amount of this product you'd like to purchase",
+            pattern: /^(?:[1-9]|[1-4][0-9]|50)$/,
+            message: "Please enter a quantity between 1 and 50",
+            required: true
+        }
+    }
+};
+
 var connection = mysql.createConnection({
-	host: "localhost",
-	port: 3306,
-	user: "root",
-	password: "",
-	database: "BamazonDB"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "BamazonDB"
 });
 
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-})
-
-connection.query('SELECT * FROM Products', function(err, res) {
-    if (err) throw err;
-    
-    for (var i = 0; i < res.length; i++) {
-    	console.log("ID: " + res[i].ID + " || Product Name: " + res[i].ProductName + " || Price: " + res[i].Price);
-    }
 });
+
+function initialProductDisplay() {
+    connection.query('SELECT * FROM Products', function(err, result) {
+        if (err) throw err;
+        console.log("\n================================================================================");
+        for (var i = 0; i < result.length; i++) {
+            console.log("--------------------------------------------------------------------------------");
+            console.log("ID: " + result[i].ID + " || Product Name: " + result[i].ProductName + " || Price: " + result[i].Price);
+        }
+    });
+};
+
+function userInput() {
+    prompt.get(schema, function(err, result) {
+        console.log("  The product you chose is: " + result.productID);
+        console.log("  The amount you'd like to purchase is: " + result.Quantity);
+        connection.query("SELECT * FROM Products WHERE ?", { id: result.productID }, function(err, specificItem) {
+            checkIfUserCanBuy(specificItem, result.Quantity);
+        })
+    });
+}
+
+function checkIfUserCanBuy(item, qtyDesired) {
+
+    if (item[0].StockQuantity < qtyDesired) {
+        console.log("Sorry; not enough in stock; please select another item:");
+        initialProductDisplay();
+        userInput();
+    }
+}
+
+initialProductDisplay();
+
+userInput();
