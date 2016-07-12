ffa0a1a165edea2d9457a01b7799630c59afa7e1
dsuare1 <suarez.derrick@gmail.com>
2016-07-12 11:49:43 -0500

    successfully updating SQL DB on order submission

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -47,8 +47,8 @@ function initialProductDisplay() {
 
 function userInput() {
     prompt.get(schema, function(err, result) {
-        console.log("  The product you chose is: " + result.productID);
-        console.log("  The amount you'd like to purchase is: " + result.Quantity);
+        // console.log("  The product you chose is: " + result.productID);
+        // console.log("  The amount you'd like to purchase is: " + result.Quantity);
         connection.query("SELECT * FROM Products WHERE ?", { id: result.productID },
             function(err, specificItem) {
                 checkIfUserCanBuy(specificItem, result.Quantity);
@@ -65,52 +65,29 @@ function checkIfUserCanBuy(item, qtyDesired) {
     } else {
         var totalWithoutTax = qtyDesired * item[0].Price;
         var totalWithTax = (totalWithoutTax * 0.0825) + totalWithoutTax;
-        console.log("Ok great!  Just to confirm...\n\nYou want " + qtyDesired + " of " + item[0].ProductName + " at $" + item[0].Price + " a piece.\n");
+        console.log("\n\nOk great!  Here's your order:\n\nYou want " + qtyDesired + " of " + item[0].ProductName + " at $" + item[0].Price + " a piece.\n");
         console.log("\nYour total cost is: \n" + "    (before tax): $" + totalWithoutTax + "\n    (after tax): $" + totalWithTax.toFixed(2) + "\n\n");
-
-        promptForConfirm();
-
-        // update DB
-        // connection.query(sqlGetQuery, function(err, result) {
-        //     if (err) throw err;
-        //     console.log("foo: " + result);
-        // });
-
-
-
-
-        // sqlUpdateQuery = "UPDATE Products SET ='',last_name='$last_name',user_city='$city_name' WHERE user_id=".$_GET['edit_id'];
+        // update the DB
+        connection.query("UPDATE Products SET ? WHERE ?", [{ StockQuantity: item[0].StockQuantity - qtyDesired }, { id: item[0].ID }], function(err, res) {
+            setTimeout(promptForContinue, 1000);
+        })
     }
 }
 
-function promptForConfirm() {
+function promptForContinue() {
     inquirer.prompt([{
         type: "list",
-        message: "Are you sure you want to purchase the above item(s)?",
-        choices: ["Yes; send them my way!", "No, I'd like to keep looking."],
-        name: "confirm"
-    }]).then(function(input) {
-        if (input.confirm === "Yes; send them my way!") {
-            console.log("\n\nGreat!  We'll start getting your order ready!\n\n");
-            inquirer.prompt([{
-                type: "list",
-                message: "Would you like to keep shopping?",
-                choices: ["Yes of course!", "No, I'm finished."],
-                name: "continue"
-            }]).then(function(cont) {
-                if (cont.continue === "Yes of course!") {
-                    console.log("Great!  Returning to item list...")
-                    setTimeout(initialProductDisplay, 2000);
-                    setTimeout(userInput, 2500);
-                } else if (cont.continue === "No, I'm finished.") {
-                    console.log("\n\nOk!  Thanks for your time :)");
-                    return;
-                }
-            });
-        } else if (input.confirm === "No, I'd like to keep looking.") {
-            console.log("Ok, no problem.  Returning to item list...")
+        message: "Would you like to keep shopping?",
+        choices: ["Yes of course!", "No, I'm finished."],
+        name: "continue"
+    }]).then(function(cont) {
+        if (cont.continue === "Yes of course!") {
+            console.log("Great!  Returning to item list...")
             setTimeout(initialProductDisplay, 2000);
             setTimeout(userInput, 2500);
+        } else if (cont.continue === "No, I'm finished.") {
+            console.log("\n\nOk!  Thanks for your time :)");
+            return;
         }
     });
 }
